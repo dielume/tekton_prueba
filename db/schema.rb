4ebcdb837852e285d05f50bd8050e7924845c39b
Diego Lume <diego.lume.a@gmail.com>
2019-01-17 17:13:47 -0500

    change in log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_172911) do
+ActiveRecord::Schema.define(version: 2019_01_17_204138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "purchase_orders", force: :cascade do |t|
+    t.string "description"
+    t.string "photo"
+    t.bigint "user_id"
+    t.string "status"
+    t.datetime "shipping_date"
+    t.datetime "reception_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_purchase_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_01_17_172911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "purchase_orders", "users"
 end
