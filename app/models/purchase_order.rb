b87190365e02d6a2e276b4606623039b8f13d951
Diego Lume <diego.lume.a@gmail.com>
2019-01-18 10:19:44 -0500

    some

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,5 +1,6 @@
 class PurchaseOrder < ApplicationRecord
   belongs_to :user
+  validates :description, :reception_date, :shipping_date, presence: true
 
   STATUS_VALUES = ["Espera", "Enviado", "Entregado"]
 end
