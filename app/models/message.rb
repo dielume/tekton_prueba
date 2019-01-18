f3203972ffb7d710c72784acc1c1e2970c518317
Diego Lume <diego.lume.a@gmail.com>
2019-01-18 16:41:58 -0500

    falta action cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,10 +7,10 @@ class Message < ApplicationRecord
 
 
   def broadcast_create_order
-    @message = message.find(self.id)
-    ActionCable.server.broadcast "chat_rooms:#{@message.chat_room.id}",
-                                {message: MessagesController.render(@message.body),
-                                 chatroom_id: @message.chat_room.id}
+    # @message = Message.find(self.id)
+    # ActionCable.server.broadcast "chat_rooms:#{@message.chat_room.id}",
+    #                             {message: MessagesController.render(@message.body),
+    #                              chatroom_id: @message.chat_room.id}
 
   end
 
