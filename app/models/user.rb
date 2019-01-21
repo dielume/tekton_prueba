3032295a73f44482073e585b85850bc0e8ea7c62
Diego Lume <diego.lume.a@gmail.com>
2019-01-20 21:59:03 -0500

    token auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,16 @@ class User < ApplicationRecord
     email
   end
 
+  def generate_auth_token
+    token = SecureRandom.hex
+    self.update_columns(auth_token: token)
+    token
+  end
+
+  def invalidate_auth_token
+    self.update_columns(auth_token: nil)
+  end
+
   private
 
   def set_default_role
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   resources :purchase_orders
   # resources :messages, only: [:create]
   resources :chat_rooms, only: [:index, :show] do
     resources :messages
   end
   devise_for :users
-  root to: "home#index"
+
+  namespace :api, :defaults => {:format => :json} do
+    as :user do
+      post   "/sign-in"       => "sessions#create"
+      delete "/sign-out"      => "sessions#destroy"
+    end
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
